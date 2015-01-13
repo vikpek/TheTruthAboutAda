8aed73f731d1ae8fa49289b110e7452b0f5790a8
puschie286 <christoph286@googlemail.com>
2015-01-13 16:33:00 +0100

    Changes

--- TruthAboutAda/Assets/Scripts/Sequenz/Scalation.cs
+++ TruthAboutAda/Assets/Scripts/Sequenz/Scalation.cs
@@ -3,18 +3,16 @@
 public class Scalation : MonoBehaviour
 {
 	[SerializeField]
-	int baseX = 1920;
-
-	[SerializeField]
-	int baseY = 1080;
+	GameObject baseLayer;
 
 	float scale;
 
 	void Awake()
 	{
-		float scaleX = (float)baseX / (float)Screen.width;
-		float scaleY = (float)baseY / (float)Screen.height;
-		scale = ( scaleX > scaleY )?( scaleX ):( scaleY );
+		Vector2 temp = baseLayer.GetComponent<RectTransform>().sizeDelta;
+		float scaleX = temp.x / (float)Screen.width;
+		float scaleY = temp.y / (float)Screen.height;
+		scale = ( scaleX >= scaleY )?( scaleX ):( scaleY );
 		foreach( Transform child in transform ) child.localScale = new Vector3( 1 / scale, 1 / scale );
 	}
 }
