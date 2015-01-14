2c41751f401fb9e5fff40abdb0173198bc48312f
Viktor Pekar <vikpek@gmail.com>
2015-01-14 12:37:42 +0100

    movement revolution

---
+++ TruthAboutAda/Assets/MovementHorizontalController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementHorizontalController : MonoBehaviour {
+
+	bool moving = false;
+
+	float minX = -10; 
+	float maxX = 7;
+
+	[SerializeField]
+	float smooth = 0.1f;
+
+	[SerializeField]
+	bool lerp = true;
+
+
+	
+	Vector3 move;
+	Vector3 startPosition;
+
+	[SerializeField]
+	bool directionRight = true;
+	
+	void Awake()
+	{
+		move = transform.localPosition;
+//		startPosition = transform.localPosition;
+
+		minX = transform.parent.FindChild("HorizontalRail").FindChild("right_border").transform.position.x;
+		maxX = transform.parent.FindChild("HorizontalRail").FindChild("left_border").transform.position.x;
+
+
+
+	}
+
+	void Update()
+	{	
+		if (!(transform.localPosition.x > minX && transform.localPosition.x < maxX)) directionRight = !directionRight;
+	
+		if (directionRight) 
+		{
+			move.x = maxX;
+		} else if (!directionRight) 
+		{
+			move.x = minX;
+		}
+
+		if (lerp) {
+				transform.localPosition = Vector3.Lerp (transform.localPosition, move, smooth * Time.deltaTime);
+		} else {
+				transform.localPosition = Vector3.MoveTowards (transform.localPosition, move, smooth * Time.deltaTime);
+		}
+
+	}
+
+	void OnTriggerEnter( Collider col )
+	{
+		directionRight = !directionRight;
+	}
+}
---
+++ TruthAboutAda/Assets/MovementVerticalController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementVerticalController : MonoBehaviour {
+
+	bool moving = false;
+
+	[SerializeField]
+	float minY = 45; 
+
+	[SerializeField]
+	float smooth = 0.1f;
+
+	Vector3 move;
+	Vector3 startPosition;
+
+	void Awake()
+	{
+		move = transform.position;
+		startPosition = transform.position;
+	}
+
+	void Update()
+	{
+		move.y = minY;
+		transform.position = Vector3.Lerp(transform.position, move, smooth * Time.deltaTime);
+	}
+}
