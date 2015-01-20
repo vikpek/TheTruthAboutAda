4296f91491d0446738094e5b1dade345d74854bc
Viktor Pekar <vikpek@gmail.com>
2015-01-20 16:26:07 +0100

    particles for wheels

--- TruthAboutAda/Assets/Scripts/Panel/PanelMovement.cs
+++ TruthAboutAda/Assets/Scripts/Panel/PanelMovement.cs
@@ -37,6 +37,8 @@ public class PanelMovement : MonoBehaviour
 		
 		if( horizontal != 0f )
 		{
+			particleSystemSideCogRight.Play();
+			particleSystemSideCogLeft.Play();
 			Vector3 nextPos = transform.position - new Vector3( horizontal * Time.fixedDeltaTime * speed, 0f );
 		
 			if( nextPos.x > maxXBorder ) nextPos.x = maxXBorder;
@@ -47,16 +49,22 @@ public class PanelMovement : MonoBehaviour
 				transformSideCogRight.rotation = CylinderUtility.Get.rotatePanelCogWheel(transformSideCogRight.transform.eulerAngles, Constants.PANEL_COGWHEEL_ROTATION_ANIMATION);
 				transformSideCogLeft.rotation = CylinderUtility.Get.rotatePanelCogWheel(transformSideCogLeft.transform.eulerAngles, Constants.PANEL_COGWHEEL_ROTATION_ANIMATION);
 
-				particleSystemSideCogLeft.Stop();
-				particleSystemSideCogRight.Play();
+//				particleSystemSideCogLeft.Stop();
+//				particleSystemSideCogRight.Play();
+				particleSystemSideCogLeft.enableEmission = false;
+				particleSystemSideCogRight.enableEmission = true;
+
 			
 			} else if(horizontal < 0)
 			{
 				transformSideCogRight.rotation = CylinderUtility.Get.rotatePanelCogWheel(transformSideCogRight.transform.eulerAngles, -Constants.PANEL_COGWHEEL_ROTATION_ANIMATION);
 				transformSideCogLeft.rotation = CylinderUtility.Get.rotatePanelCogWheel(transformSideCogRight.transform.eulerAngles, -Constants.PANEL_COGWHEEL_ROTATION_ANIMATION);
 		
-				particleSystemSideCogLeft.Play();
-				particleSystemSideCogRight.Stop ();
+				particleSystemSideCogLeft.enableEmission = true;
+				particleSystemSideCogRight.enableEmission = false;
+
+//				particleSystemSideCogLeft.Play();
+//				particleSystemSideCogRight.Stop ();
 			}
 			rigidbody.MovePosition( nextPos );
 		}
