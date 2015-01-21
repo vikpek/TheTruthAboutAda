4dc111ab79250499430f1ac3101e46cd2ce8f0ea
puschie286 <christoph286@googlemail.com>
2015-01-21 19:48:48 +0100

    add multiplayer, fix animation bugs

--- TruthAboutAda/Assets/Scripts/Panel/PanelShooting.cs
+++ TruthAboutAda/Assets/Scripts/Panel/PanelShooting.cs
@@ -13,6 +13,9 @@ public class PanelShooting : MonoBehaviour
 	[SerializeField]
 	float waitBetweenDifferentsShots = 0.2f;
 
+	[SerializeField]
+	int multi;
+
 	SoundManager soundManager;
 
 	float shootWaiter;
@@ -25,7 +28,7 @@ public class PanelShooting : MonoBehaviour
 	void Awake()
 	{
 		soundManager = GameObject.FindGameObjectWithTag( Tags.GAMECONTROLLER ).GetComponent<SoundManager>();
-		delayController = GameObject.FindGameObjectWithTag( Tags.DELAYBAR ).GetComponent<DelayController>();
+		delayController = transform.Find( "DelayCylinder" ).GetComponent<DelayController>();
 	}
 
 	void Update()
@@ -68,19 +71,54 @@ public class PanelShooting : MonoBehaviour
 
 	int scanKeys() 
 	{
-		if( Input.GetKeyDown( KeyCode.Alpha0 ) || Input.GetKeyDown( KeyCode.Keypad0 ) ) return 0;
-		if( Input.GetKeyDown( KeyCode.Alpha1 ) || Input.GetKeyDown( KeyCode.Keypad1 ) ) return 1;
-		if( Input.GetKeyDown( KeyCode.Alpha2 ) || Input.GetKeyDown( KeyCode.Keypad2 ) ) return 2;
-		if( Input.GetKeyDown( KeyCode.Alpha3 ) || Input.GetKeyDown( KeyCode.Keypad3 ) ) return 3;
-		if( Input.GetKeyDown( KeyCode.Alpha4 ) || Input.GetKeyDown( KeyCode.Keypad4 ) ) return 4;
-		if( Input.GetKeyDown( KeyCode.Alpha5 ) || Input.GetKeyDown( KeyCode.Keypad5 ) ) return 5;
-		if( Input.GetKeyDown( KeyCode.Alpha6 ) || Input.GetKeyDown( KeyCode.Keypad6 ) ) return 6;
-		if( Input.GetKeyDown( KeyCode.Alpha7 ) || Input.GetKeyDown( KeyCode.Keypad7 ) ) return 7;
-		if( Input.GetKeyDown( KeyCode.Alpha8 ) || Input.GetKeyDown( KeyCode.Keypad8 ) ) return 8;
-		if( Input.GetKeyDown( KeyCode.Alpha9 ) || Input.GetKeyDown( KeyCode.Keypad9 ) ) return 9;
-		if( Input.GetKeyDown( KeyCode.Space ) || Input.GetKeyDown( KeyCode.KeypadEnter ) || Input.GetKeyDown( KeyCode.Return ) || Input.GetKeyDown( KeyCode.UpArrow ) || Input.GetMouseButtonDown( 0 ) ) return 10;
-		if( Input.GetKeyDown( KeyCode.LeftArrow ) || Input.GetAxisRaw("Mouse ScrollWheel") < 0 ) return 11;
-		if( Input.GetKeyDown( KeyCode.RightArrow ) || Input.GetAxisRaw("Mouse ScrollWheel") > 0 ) return 12;
+		switch( multi )
+		{
+			case 0 :
+				if( Input.GetKeyDown( KeyCode.Alpha0 ) || Input.GetKeyDown( KeyCode.Keypad0 ) ) return 0;
+				if( Input.GetKeyDown( KeyCode.Alpha1 ) || Input.GetKeyDown( KeyCode.Keypad1 ) ) return 1;
+				if( Input.GetKeyDown( KeyCode.Alpha2 ) || Input.GetKeyDown( KeyCode.Keypad2 ) ) return 2;
+				if( Input.GetKeyDown( KeyCode.Alpha3 ) || Input.GetKeyDown( KeyCode.Keypad3 ) ) return 3;
+				if( Input.GetKeyDown( KeyCode.Alpha4 ) || Input.GetKeyDown( KeyCode.Keypad4 ) ) return 4;
+				if( Input.GetKeyDown( KeyCode.Alpha5 ) || Input.GetKeyDown( KeyCode.Keypad5 ) ) return 5;
+				if( Input.GetKeyDown( KeyCode.Alpha6 ) || Input.GetKeyDown( KeyCode.Keypad6 ) ) return 6;
+				if( Input.GetKeyDown( KeyCode.Alpha7 ) || Input.GetKeyDown( KeyCode.Keypad7 ) ) return 7;
+				if( Input.GetKeyDown( KeyCode.Alpha8 ) || Input.GetKeyDown( KeyCode.Keypad8 ) ) return 8;
+				if( Input.GetKeyDown( KeyCode.Alpha9 ) || Input.GetKeyDown( KeyCode.Keypad9 ) ) return 9;
+				if( Input.GetKeyDown( KeyCode.Space ) || Input.GetKeyDown( KeyCode.KeypadEnter ) || Input.GetKeyDown( KeyCode.Return ) || Input.GetKeyDown( KeyCode.UpArrow ) || Input.GetMouseButtonDown( 0 ) ) return 10;
+				if( Input.GetKeyDown( KeyCode.LeftArrow ) || Input.GetAxisRaw("Mouse ScrollWheel") < 0 ) return 11;
+				if( Input.GetKeyDown( KeyCode.RightArrow ) || Input.GetAxisRaw("Mouse ScrollWheel") > 0 ) return 12;
+				break;
+			case 1 :
+				if( Input.GetKeyDown( KeyCode.Alpha0 ) ) return 0;
+				if( Input.GetKeyDown( KeyCode.Alpha1 ) ) return 1;
+				if( Input.GetKeyDown( KeyCode.Alpha2 ) ) return 2;
+				if( Input.GetKeyDown( KeyCode.Alpha3 ) ) return 3;
+				if( Input.GetKeyDown( KeyCode.Alpha4 ) ) return 4;
+				if( Input.GetKeyDown( KeyCode.Alpha5 ) ) return 5;
+				if( Input.GetKeyDown( KeyCode.Alpha6 ) ) return 6;
+				if( Input.GetKeyDown( KeyCode.Alpha7 ) ) return 7;
+				if( Input.GetKeyDown( KeyCode.Alpha8 ) ) return 8;
+				if( Input.GetKeyDown( KeyCode.Alpha9 ) ) return 9;
+				if( Input.GetKeyDown( KeyCode.Space ) || Input.GetKeyDown( KeyCode.Return ) ) return 10;
+				if( Input.GetKeyDown( KeyCode.LeftArrow ) ) return 11;
+				if( Input.GetKeyDown( KeyCode.RightArrow ) ) return 12;
+				break;
+			case 2 :
+				if( Input.GetKeyDown( KeyCode.Keypad0 ) ) return 0;
+				if( Input.GetKeyDown( KeyCode.Keypad1 ) ) return 1;
+				if( Input.GetKeyDown( KeyCode.Keypad2 ) ) return 2;
+				if( Input.GetKeyDown( KeyCode.Keypad3 ) ) return 3;
+				if( Input.GetKeyDown( KeyCode.Keypad4 ) ) return 4;
+				if( Input.GetKeyDown( KeyCode.Keypad5 ) ) return 5;
+				if( Input.GetKeyDown( KeyCode.Keypad6 ) ) return 6;
+				if( Input.GetKeyDown( KeyCode.Keypad7 ) ) return 7;
+				if( Input.GetKeyDown( KeyCode.Keypad8 ) ) return 8;
+				if( Input.GetKeyDown( KeyCode.Keypad9 ) ) return 9;
+				if( Input.GetKeyDown( KeyCode.KeypadEnter ) || Input.GetMouseButtonDown( 0 ) ) return 10;
+				if( Input.GetAxisRaw("Mouse ScrollWheel") < 0 ) return 11;
+				if( Input.GetAxisRaw("Mouse ScrollWheel") > 0 ) return 12;
+				break;
+		}
 		return -1;
 	}
 }
