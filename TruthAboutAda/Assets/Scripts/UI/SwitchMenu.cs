f52eb291b6aadb0d63d24ee1695fe54e92ed3066
puschie286 <christoph286@googlemail.com>
2015-01-22 15:31:15 +0100

    Added MenuSwitch

---
+++ TruthAboutAda/Assets/Scripts/UI/SwitchMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SwitchMenu : MonoBehaviour
+{
+	[SerializeField]
+	float time = 1f;
+
+	void Update()
+	{
+		time -= Time.deltaTime;
+		if( time <= 0f ) Application.LoadLevel( Constants.LEVEL_MENU );
+	}
+}
