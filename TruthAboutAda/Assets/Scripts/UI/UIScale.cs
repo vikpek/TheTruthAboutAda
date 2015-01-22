91b93b12d7c3f8983ac0705feb1c1850595cd2a9
puschie286 <christoph286@googlemail.com>
2015-01-22 01:37:21 +0100

    Replace Mainmenu( old graphics )

---
+++ TruthAboutAda/Assets/Scripts/UI/UIScale.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScale : MonoBehaviour
+{
+	[SerializeField]
+	GameObject baseLayer;
+	
+	float scale;
+	
+	void Awake()
+	{
+		Vector2 temp = baseLayer.GetComponent<RectTransform>().sizeDelta;
+		GetComponent<UnityEngine.UI.CanvasScaler>().matchWidthOrHeight = ( temp.x / (float)Screen.width >= temp.y / (float)Screen.height )?( 0f ):( 1f );
+	}
+}
