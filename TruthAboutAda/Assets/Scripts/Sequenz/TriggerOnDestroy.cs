e9f668f85c8700f4f5c8a270efd1e920a74e622e
puschie286 <christoph286@googlemail.com>
2015-01-29 12:11:44 +0100

    fixed TriggerOnDestroy for explosions

--- TruthAboutAda/Assets/Scripts/Sequenz/TriggerOnDestroy.cs
+++ TruthAboutAda/Assets/Scripts/Sequenz/TriggerOnDestroy.cs
@@ -2,10 +2,23 @@
 
 public class TriggerOnDestroy : MonoBehaviour
 {
+	Transform _ref;
+
 	[SerializeField]
 	GameObject target;
 
-	void OnDestroy()
+	void Awake()
+	{
+		_ref = transform.Find( Constants.ANIMATION_HOLDER );
+	}
+
+	void Update()
+	{
+		if( _ref == null ) Trigger();
+		else if( _ref.parent != transform ) Trigger();
+	}
+
+	void Trigger()
 	{
 		Debug.Log("Trigger");
 		target.SetActive( true );
