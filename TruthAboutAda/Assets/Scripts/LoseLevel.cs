92f94551d66aff2868e55db6e86c60f1a456f3f6
Viktor Pekar <vikpek@gmail.com>
2015-02-02 12:17:06 +0100

    added story text and some poses

--- TruthAboutAda/Assets/Scripts/LoseLevel.cs
+++ TruthAboutAda/Assets/Scripts/LoseLevel.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class LoseLevel : MonoBehaviour
 {
 
+	UIController uiController;
+	bool _lock;
+
+	void Awake(){
+		uiController = GameObject.FindWithTag( Tags.GAMECONTROLLER ).GetComponent<UIController>();
+	}
+
 	void OnCollisionEnter( Collision col )
 	{
-		if( col.gameObject.tag == Tags.CREEP ) {
+		if( col.gameObject.tag == Tags.CREEP  && !_lock) {
+			_lock = true;
 			transform.GetComponent<TheEventNoTime>().enabled = true;
-//			StartCoroutine(waitAndLoose(3f));
+			StartCoroutine(waitAndLoose(3f));
 		}
 	}
 
 	IEnumerator waitAndLoose( float time )
 	{
 		yield return new WaitForSeconds( time );
-		GameObject.FindWithTag( Tags.GAMECONTROLLER ).GetComponent<UIController>().SetGameOver();
+		uiController.BackToMenu();
 
 	}
 }
