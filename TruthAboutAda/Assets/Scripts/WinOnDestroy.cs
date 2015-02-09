e05575b49d1c8bb76f77c516d2a2c4a02949f124
puschie286 <christoph286@googlemail.com>
2015-02-09 13:04:07 +0100

    Update WinOnDestroy

--- TruthAboutAda/Assets/Scripts/WinOnDestroy.cs
+++ TruthAboutAda/Assets/Scripts/WinOnDestroy.cs
@@ -5,10 +5,12 @@ public class WinOnDestroy : MonoBehaviour
 	[SerializeField]
 	int setLevelOnWin = -1; // -1 for disable
 
+	[SerializeField]
+	GameObject target;
+
 	void OnDestroy()
 	{
-		GameObject temp = GameObject.FindGameObjectWithTag( Tags.GAMECONTROLLER );
-		if( temp != null )temp.GetComponent<UIController>().SetWin();
+		if( target != null ) target.SetActive( true );
 		if( setLevelOnWin != -1 ) LevelProgress.Get.SetLevel( setLevelOnWin );
 	}
 }
