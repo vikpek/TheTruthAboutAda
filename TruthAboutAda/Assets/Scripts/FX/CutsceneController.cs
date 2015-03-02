a6121a7a7ff269abd01c96616496c3546ea20ca2
Viktor Pekar <vikpek@gmail.com>
2015-03-02 15:45:20 +0100

    towards highscore

--- TruthAboutAda/Assets/Scripts/FX/CutsceneController.cs
+++ TruthAboutAda/Assets/Scripts/FX/CutsceneController.cs
@@ -4,22 +4,22 @@ using System.Collections;
 public class CutsceneController : MonoBehaviour
 {
 
-	[SerializeField]
-	MovieTexture movieTexture;
-
-
-	void Start () 
-	{
-		gameObject.renderer.material.mainTexture = movieTexture;
-		movieTexture.Play ();
-	}
-
-	void Update()
-	{
-		if( !movieTexture.isPlaying )
-		{
-			Application.LoadLevel(Constants.LEVEL_LEVEL01);
-		}
-
-	}
+//	[SerializeField]
+//	MovieTexture movieTexture;
+//
+//
+//	void Start () 
+//	{
+//		gameObject.renderer.material.mainTexture = movieTexture;
+//		movieTexture.Play ();
+//	}
+//
+//	void Update()
+//	{
+//		if( !movieTexture.isPlaying )
+//		{
+//			Application.LoadLevel(Constants.LEVEL_LEVEL01);
+//		}
+//
+//	}
 }
--- TruthAboutAda/Assets/Scripts/Utilities/HighScoreManager.cs
+++ TruthAboutAda/Assets/Scripts/Utilities/HighScoreManager.cs
@@ -69,4 +69,36 @@ public class HighScoreManager : MonoBehaviour
 	{
 		return (int) currentMultiplier;
 	}
+
+	public void addHighscore(string name, int score)
+	{
+		int newScore;
+		string newName;
+		int oldScore;
+		string oldName;
+
+		newScore = score;
+		newName = name;
+
+		int i;
+		for(i=0;i<10;i++){
+			if(PlayerPrefs.HasKey(i+"HScore")){
+				if(PlayerPrefs.GetInt(i+"HScore")<newScore){ 
+					// new score is higher than the stored score
+					oldScore = PlayerPrefs.GetInt(i+"HScore");
+					oldName = PlayerPrefs.GetString(i+"HScoreName");
+					PlayerPrefs.SetInt(i+"HScore",newScore);
+					PlayerPrefs.SetString(i+"HScoreName",newName);
+					newScore = oldScore;
+					newName = oldName;
+				}
+			}else{
+				PlayerPrefs.SetInt(i+"HScore",newScore);
+				PlayerPrefs.SetString(i+"HScoreName",newName);
+				newScore = 0;
+				newName = "";
+			}
+		}
+
+	}
 }
